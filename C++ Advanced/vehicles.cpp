f569874b70707137caa2ce1996110f81bfcce02f
c-gilligan <cgilligan@reed.edu>
2022-12-14 18:11:35 -0800

    Added methods to Bus class

--- C++ Advanced/vehicles.cpp
+++ C++ Advanced/vehicles.cpp
@@ -4,7 +4,45 @@
 
 class Bus : public Car {
 public: 
+	vector<int> generate_route(int starting_stop) {
 
+	}
+
+	bool move_to(double new_x, double new_y) {
+		double distance;
+		double required_fuel;
+
+		distance = sqrt( pow(new_x - (this -> pos_x), 2) + pow(new_y - (this -> pos_y), 2));
+		
+		// Incorporate mileage penalty per passenger into fuel calculations
+		total_penalty = mileage_penalty * (this -> stop_numbers.size());
+		required_fuel = distance / ((this -> fuel_efficiency) + total_penalty);
+
+		if (required_fuel <= (this -> fuel)) {
+			// Move car to destination
+			this -> pos_x = new_x;
+			this -> pos_y = new_y;
+
+			// Subtract appropriate fuel amount
+			this -> fuel -= required_fuel;
+
+			return true;
+		} else return false;
+	}
+
+	bool enough_fuel(double new_x, double new_y) {
+		double distance;
+		double required_fuel;
+
+		distance = sqrt( pow(new_x - (this -> pos_x), 2) + pow(new_y - (this -> pos_y), 2));
+		
+		// Incorporate mileage penalty per passenger into fuel calculations
+		total_penalty = mileage_penalty * (this -> stop_numbers.size());
+		required_fuel = distance / ((this -> fuel_efficiency) + total_penalty);
+
+		if (required_fuel <= (this -> fuel)) return true;
+		else return false;
+	}
 
 private:
 	int max_passengers;
