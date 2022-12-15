3d4ef9246d0fe1df6f43ea53482d3b59ef2274c1
c-gilligan <cgilligan@reed.edu>
2022-12-14 20:12:26 -0800

    Added Ambulance class

--- C++ Advanced/vehicles.cpp
+++ C++ Advanced/vehicles.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-class Bus : public Car {
+class Bus : protected Car {
 public: 
 
 	bool move_to(double new_x, double new_y) {
@@ -133,4 +133,34 @@ private:
 	// Used string rather than vector for patient condition because the instructions
 	// said condition (singular)
 	map<string, string> patients;
+}
+
+class Ambulance : protected Car, protected MedicalCenter {
+public:
+	bool move_to(double new_x, double new_y) {
+		double distance;
+		double required_fuel;
+
+		distance = sqrt( pow(new_x - (this -> pos_x), 2) + pow(new_y - (this -> pos_y), 2));
+		
+		// Incorporate mileage penalty per passenger into fuel calculations
+		total_penalty = mileage_penalty * ((this -> patients.size()) + (this -> providers.size()));
+		required_fuel = distance / ((this -> fuel_efficiency) + total_penalty);
+
+		if (required_fuel <= (this -> fuel)) {
+			// Move car to destination
+			this -> pos_x = new_x;
+			this -> pos_y = new_y;
+
+			// Subtract appropriate fuel amount
+			this -> fuel -= required_fuel;
+
+			return true;
+		} else return false;
+	}
+
+private:
+	int max_patients;
+	int max_providers;
+	int mileage_penalty;
 }
