955e1f072a40b24f15a21c95e741670218e91094
c-gilligan <cgilligan@reed.edu>
2022-12-14 17:49:15 -0800

    Created new files for vehicles question

---
+++ C++ Advanced/vehicles.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <memory>
+#include "cars.hh"
+
+class Bus : public Car {
+public: 
+
+
+private:
+	int max_passengers;
+	vector<int> stop_numbers;
+	double mileage_penalty;
+
+}
